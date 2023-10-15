1147fad0bfdce765b9baa2de21be349c6c336602
Hui Teo <hui.teo@kcl.ac.uk>
2023-10-15 15:31:35 +0100

    refine Thing model

--- thingproject/models.py
+++ thingproject/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 class Thing(models.Model):
-    name = models.CharField()
-    description = models.CharField()
-    quantity = models.IntegerField()
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    description = models.CharField(max_length=120, unique=False, blank=False)
+    quantity = models.PositiveSmallIntegerField(validators=[
+        MaxValueValidator(100)
+    ])
