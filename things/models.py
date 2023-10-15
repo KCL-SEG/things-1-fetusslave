73298e24f78e478607caa6817d6cece3c2710220
Hui Teo <hui.teo@kcl.ac.uk>
2023-10-15 15:42:32 +0100

    add min value validator to quantity field

--- things/models.py
+++ things/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-from django.core.validators import MaxValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
     description = models.CharField(max_length=120, unique=False, blank=False)
     quantity = models.PositiveSmallIntegerField(validators=[
+        MinValueValidator(0),
         MaxValueValidator(100)
     ])
