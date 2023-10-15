2f3d242c962122e00ce688e08634357b0f675721
Hui Teo <hui.teo@kcl.ac.uk>
2023-10-15 15:43:30 +0100

    make description able to be blank

--- things/models.py
+++ things/models.py
@@ -3,7 +3,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
-    description = models.CharField(max_length=120, unique=False, blank=False)
+    description = models.CharField(max_length=120, unique=False, blank=True)
     quantity = models.PositiveSmallIntegerField(validators=[
         MinValueValidator(0),
         MaxValueValidator(100)
